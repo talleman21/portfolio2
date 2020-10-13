2b9af7431a862a7659ce63ff7078d41b865b9ce2
talleman <talleman21@gmail.com>
2020-10-12 20:56:29 -0700

    update image names

--- src/shared/projectInfo.tsx
+++ src/shared/projectInfo.tsx
@@ -3,7 +3,7 @@ const projectInfo = [
     title:'Thunderhead Design',
     type:'Front-End',
     description:'Graphic Design portfolio site, that utilizes React,Sass and Bootstrap.  Form submission and Email handling is taken care of with the Formspree api.',
-    technologies:['React','Bootstrap','Sass'],
+    technologies:['react','bootstrap','sass'],
     github:'https://github.com/talleman21',
     live:'https://www.thunderheaddesign.net',
     image:'thead-format.png'
@@ -12,7 +12,7 @@ const projectInfo = [
     title:'Authentication Server',
     type:'Full-Stack',
     description:'JWT based authentication server.  Front-End configured with React, Back-end configured with Node.js, MongoDB, and express',
-    technologies:['MongoDB','Express.js','React','Node.js'],
+    technologies:['mongodb','express.js','react','node.js'],
     github:'https://github.com/talleman21',
     live:'',
     image:'auth-format.png'
@@ -21,7 +21,7 @@ const projectInfo = [
     title:'Bootcamp API',
     type:'Back-End',    
     description:'A Public bootcamp api to serve json, and images.  Uses Node.js with Express, and MongoDB.  This is a public server, not meant to store info indefinitely, so all data is set back to default after 15 minutes of no activity',
-    technologies:['Node.js','Express.js','MongoDB'],
+    technologies:['node.js','express.js','mongodb'],
     github:'https://github.com/talleman21',
     live:'',
     image:''
